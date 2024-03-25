7769d8fcb3e67ad6dfb64362b284353182ae370c
Lucas Wilkinson <lwilkinson@neuralmagic.com>
2024-03-25 04:27:43 +0000

    fix formatting

--- neuralmagic/tools/profiler/print_table.py
+++ neuralmagic/tools/profiler/print_table.py
@@ -1,19 +1,9 @@
 import argparse
 import json
-import pandas as pd
-import copy
 
-from collections import defaultdict
-from dataclasses import dataclass, field, asdict
 from vllm.profiler.nm_profile import SummaryStatsEntry, ModelStatsEntry
-from vllm.profiler.utils import (indent_string, TablePrinter, event_has_module,
-                                 event_is_torch_op, event_module_repr,
-                                 event_torch_op_stack_trace)
-from typing import Dict, List, Union, Optional, Tuple, Callable, TypeAlias
-from torch.profiler import profile, ProfilerActivity
-from torch.autograd.profiler import FunctionEvent
-from torch._C._autograd import _ProfilerResult, _KinetoEvent, DeviceType
-from torch._C._profiler import _EventType, _ProfilerEvent, _ExperimentalConfig
+from vllm.profiler.utils import indent_string, TablePrinter
+from typing import Dict
 
 
 def flatten_entries(entry_cls, profile_dict: Dict):
@@ -32,43 +22,6 @@ def flatten_entries(entry_cls, profile_dict: Dict):
         get_entries(root)
 
     return entries_and_depth
-
-
-def print_summary_table(rows, column_widths: Dict[str, int] = None):
-    _column_widths = dict(name=80,
-                          cuda_time_us=12,
-                          pct_cuda_time=12,
-                          invocations=15)
-    if column_widths:
-        _column_widths.update(**column_widths)
-    filtered_summary_table = [
-        (depth, row)
-        for depth, row in self._flatten_stats_tree(self._summary_stats_tree)
-        if row.cuda_time_us > 0
-    ]
-    TablePrinter(SummaryStatsEntry, _column_widths).print_table(
-        self._indent_row_names_based_on_depth(
-            filtered_summary_table,
-            indent_style=lambda indent: "|" + "-" * indent + " "))
-
-
-def print_model_table(self, column_widths: Dict[str, int] = None):
-    _column_widths = dict(name=60,
-                          cpu_time_us=12,
-                          cuda_time_us=12,
-                          pct_cuda_time=12,
-                          trace=60)
-    if column_widths:
-        _column_widths.update(**column_widths)
-    filtered_model_table = [
-        (depth, row)
-        for depth, row in self._flatten_stats_tree(self._model_stats_tree)
-        if row.cuda_time_us > 0 or row.cpu_time_us > 0
-    ]
-    TablePrinter(ModelStatsEntry, _column_widths).print_table(
-        self._indent_row_names_based_on_depth(
-            filtered_model_table,
-            indent_style=lambda indent: "|" + "-" * indent + " "))
 
 
 if __name__ == "__main__":
