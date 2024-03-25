8ecb124a1478411f9e4c302dba842e0a2e86948f
Lucas Wilkinson <lwilkinson@neuralmagic.com>
2024-03-25 03:54:40 +0000

    review comments

--- examples/offline_profile.py
+++ examples/offline_profile.py
@@ -16,15 +16,15 @@ MAX_SEQ_LEN_DEFAULT = 1024
 
 @dataclass
 class ProfileContext:
-    model_name: str
+    model: str
     model_revision: str
-    is_sparse: bool
-    quant_method: str
+    sparsity: str
+    quantization: str
     max_seq_len: int
     max_num_batched_tokens: int
     prompt_len: int
     batch_size: int
-    num_gpus: int
+    tensor_parallel_size: int
     allow_cuda_graphs: bool
 
 
@@ -39,14 +39,14 @@ def run_profile(context: ProfileContext, csv_output: Optional[str],
 
     # Create LLM
     llm = LLM(
-        model=context.model_name,
+        model=context.model,
         revision=context.model_revision,
-        sparsity="sparse_w16a16" if context.is_sparse else None,
+        sparsity=context.sparsity,
         enforce_eager=not context.allow_cuda_graphs,
-        tensor_parallel_size=context.num_gpus,
+        tensor_parallel_size=context.tensor_parallel_size,
         gpu_memory_utilization=0.9,
         max_model_len=context.max_seq_len,
-        quantization=context.quant_method,
+        quantization=context.quantization,
         max_num_batched_tokens=context.max_num_batched_tokens,
     )
 
@@ -157,11 +157,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
-        "--model_name",
+        "--model",
         type=str,
         required=True,
         help='The name or path of a HuggingFace Transformers model.')
-    parser.add_argument("--model_revision", type=str, default=None)
+    parser.add_argument("--model-revision", type=str, default=None)
     parser.add_argument(
         "--csv",
         type=str,
@@ -177,47 +177,54 @@ if __name__ == "__main__":
         default=None,
         help="Export the results as a json file. This should be the filename")
     parser.add_argument(
-        "--is_sparse",
-        action='store_true',
-        help=
-        "Use unstructured sparse kernels, sets sparse config to sparse_W16A16")
-    parser.add_argument(
-        "--quant_method",
+        "--sparsity",
+        "-s",
         type=str,
+        choices=[None, 'sparse_w16a16', 'semi_structured_sparse_w16a16'],
+        help="Method used to compress sparse weights. If "
+        "None, we first check the `sparsity_config` attribute"
+        "in the model config file. If that is None we assume"
+        "the model weights are dense")
+    parser.add_argument(
+        "--quantization",
+        "-q",
+        type=str,
+        choices=['awq', 'gptq', 'squeezellm', 'marlin', None],
         default=None,
         help="The method used to quantize the model weights, "
         "options are \"marlin\", \"awq\", \"gptq\" and \"squeezellm\"")
     parser.add_argument(
-        "--max_seq_len",
+        "--max-seq-len",
         type=int,
         default=MAX_SEQ_LEN_DEFAULT,
         help=f"Maximum length of a sequence (including prompt and output), "
         f"default={MAX_SEQ_LEN_DEFAULT}")
     parser.add_argument(
-        "--max_num_batched_tokens",
+        "--max-num-batched-tokens",
         type=int,
         default=None,
         help="Maximum number of tokens to be processed in a single iteration. "
-        " Should be greater than batch_size * prompt_len so the prefill can "
+        " Should be greater than batch-size * prompt-len so the prefill can "
         " run in a single iteration.")
     parser.add_argument(
-        "--prompt_len",
+        "--prompt-len",
         type=int,
         default=PROMPT_LEN_DEFAULT,
         help=f"Length of the random prompt to use when profiling, all batched "
         f"requests use the same prompt_len, default={PROMPT_LEN_DEFAULT}")
-    parser.add_argument("--batch_size",
+    parser.add_argument("--batch-size",
                         type=int,
                         default=BATCH_SIZE_DEFAULT,
                         help=f"Number of requests to run as a single batch, "
                         f"default={BATCH_SIZE_DEFAULT}")
-    parser.add_argument("--num_gpus",
+    parser.add_argument("--tensor-parallel-size",
+                        "-tp",
                         type=int,
                         default=1,
                         help="Number of GPUs to use i.e. tensor parallelism, "
                         "default=1")
     parser.add_argument(
-        '--allow_cuda_graphs',
+        "--allow-cuda-graphs",
         action='store_true',
         help="Enables cuda graphs to be used, well remove a lot of the module "
         "level info in the profiler results since almost everything runs in "
