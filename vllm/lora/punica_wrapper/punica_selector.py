6a1a1fdd67781c4d0be07b7a0182dca710e41fd5
Varun Sundar Rabindranath <varun@neuralmagic.com>
2025-01-10 15:43:07 -0500

    plumb punica selector

--- vllm/lora/punica_wrapper/punica_selector.py
+++ vllm/lora/punica_wrapper/punica_selector.py
@@ -2,6 +2,7 @@ from vllm.logger import init_logger
 from vllm.platforms import current_platform
 
 from .punica_base import PunicaWrapperBase
+import vllm.envs as envs
 
 logger = init_logger(__name__)
 
@@ -9,9 +10,14 @@ logger = init_logger(__name__)
 def get_punica_wrapper(*args, **kwargs) -> PunicaWrapperBase:
     if current_platform.is_cuda_alike():
         # Lazy import to avoid ImportError
-        from vllm.lora.punica_wrapper.punica_gpu import PunicaWrapperGPU
-        logger.info_once("Using PunicaWrapperGPU.")
-        return PunicaWrapperGPU(*args, **kwargs)
+        if envs.VLLM_USE_V1:
+            from vllm.lora.punica_wrapper.v1_punica_gpu import V1PunicaWrapperGPU
+            logger.info_once("Using V1PunicaWrapperGPU.")
+            return V1PunicaWrapperGPU(*args, **kwargs)
+        else:
+            from vllm.lora.punica_wrapper.punica_gpu import PunicaWrapperGPU
+            logger.info_once("Using PunicaWrapperGPU.")
+            return PunicaWrapperGPU(*args, **kwargs)
     elif current_platform.is_hpu():
         # Lazy import to avoid ImportError
         from vllm.lora.punica_wrapper.punica_hpu import PunicaWrapperHPU
