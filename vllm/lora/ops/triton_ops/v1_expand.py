4b8b3abfac0fb74438abd35ba1e7458ac17e930e
Varun Sundar Rabindranath <varun@neuralmagic.com>
2025-01-19 18:24:07 -0500

    add v1 kernels
    Signed-off-by: Varun Sundar Rabindranath <varun@neuralmagic.com>

---
+++ vllm/lora/ops/triton_ops/v1_expand.py
@@ -0,0 +1,261 @@
+"""
+Based on:
+Chen, L., Ye, Z., Wu, Y., Zhuo, D., Ceze, L., & Krishnamurthy, A. (2023).
+Punica: Multi-Tenant LoRA Serving.
+https://arxiv.org/abs/2310.18547
+"""
+
+from typing import List
+
+import torch
+import triton
+import triton.language as tl
+
+from vllm.utils import direct_register_custom_op
+
+from .utils import _get_lora_b_ptr
+from .kernel_utils import do_expand_kernel
+
+
+@triton.jit
+def _v1_expand_kernel(
+        input_ptr,
+        lora_ptr,
+        out_ptr,
+        M,
+        N,
+        K,
+        token_indices_sorted_by_lora_ids,
+        num_tokens_per_lora,
+        lora_token_start_loc,
+        lora_ids,
+        slice_start_loc,
+        input_d0_stride,
+        input_d1_stride,
+        input_d2_stride,  # 1
+        ls_d0_ptr,
+        ls_d1_ptr,
+        ls_d2_ptr,  # 1
+        output_d0_stride,
+        output_d1_stride,  # 1
+        output_hs_ptr,
+        BLOCK_M: tl.constexpr,
+        BLOCK_N: tl.constexpr,
+        BLOCK_K: tl.constexpr,
+        EVEN_K: tl.constexpr,
+        ADD_INPUTS: tl.constexpr,
+        CAST_TYPE: tl.constexpr,
+        SLICE_NUM: tl.constexpr,
+        SAME_STRIDE: tl.constexpr):
+    """
+
+    Similar to the 'sgmv_expand' operator, but with an added parameter
+    'slice_offset'. The reason for not reusing the 'sgmv_expand' operator
+    might be that in the future, we could implement a fusion operator to
+    achieve the current functionality instead of having to call it multiple
+    times.
+    """
+
+    pid_lmn = tl.program_id(axis=0)
+    slice_id = tl.program_id(axis=1)
+
+    cta_n_num = tl.cdiv(N, BLOCK_N)
+    cta_m_num = tl.cdiv(M, BLOCK_M)
+
+    lora_idx = pid_lmn // (cta_m_num * cta_n_num)
+    pid_n = (pid_lmn // cta_m_num) % cta_n_num
+    pid_m = pid_lmn % cta_m_num
+
+    lora_id = tl.load(lora_ids + lora_idx)
+    if lora_id == -1:
+        # early exit for the no-lora case.
+        return
+
+    # lora m indices offsets
+    lora_m_indices_start = tl.load(lora_token_start_loc + lora_idx)
+    lora_m_size = tl.load(num_tokens_per_lora + lora_idx)
+
+    cta_m_offset = pid_m * BLOCK_M
+    if cta_m_offset >= lora_m_size:
+        # early exit CTA
+        return
+
+    # When the output dimensions of each slice are the same,cur_n=N, otherwise
+    # cur_n=tl.load(output_hs_ptr + slice_id), this situation exists in GQA's
+    # qkv linear.
+    curr_N = N if SAME_STRIDE else tl.load(output_hs_ptr + slice_id)
+    if pid_n * BLOCK_N > curr_N:
+        return
+
+    CTA_M_LEN = min(BLOCK_M, lora_m_size - cta_m_offset)
+    offset_m = tl.arange(0, BLOCK_M) % CTA_M_LEN 
+
+    cta_lora_seq_indices = (token_indices_sorted_by_lora_ids +
+                            lora_m_indices_start + cta_m_offset)
+    ram = tl.load(cta_lora_seq_indices + offset_m)
+
+    do_expand_kernel(pid_m,
+                     pid_n,
+                     lora_id,
+                     slice_id,
+                     input_ptr,
+                     lora_ptr,
+                     out_ptr,
+                     N,
+                     K,
+                     CTA_M_LEN,
+                     ram, # array identifying the rows of Input ptr to operate on
+                     slice_start_loc,
+                     # input ptr strides
+                     input_d0_stride,
+                     input_d1_stride,
+                     input_d2_stride,
+                     # lora ptr strides
+                     ls_d0_ptr,
+                     ls_d1_ptr,
+                     ls_d2_ptr,
+                     # out ptr strides
+                     output_d0_stride,
+                     output_d1_stride,
+                     # constants
+                     BLOCK_M,
+                     BLOCK_N,
+                     BLOCK_K,
+                     SAME_STRIDE,
+                     SLICE_NUM,
+                     EVEN_K,
+                     CAST_TYPE,
+                     ADD_INPUTS)
+
+@torch.inference_mode()
+def _v1_expand(
+    inputs: torch.Tensor,
+    lora_b_weights: List[torch.Tensor],
+    output_tensor: torch.Tensor,
+    token_lora_mapping: torch.Tensor,
+    token_indices_sorted_by_lora_ids: torch.Tensor,  # inputs.size(0)
+    num_tokens_per_lora: torch.Tensor,  # max-loras + 1
+    lora_token_start_loc: torch.Tensor,  # max-loras + 2
+    lora_ids: torch.Tensor,  # max-loras + 1
+    offset_start: int = 0,
+    add_inputs: bool = False,
+) -> None:
+    """
+    Args:
+        inputs (torch.Tensor): input tensor
+        lora_b_weights (List[torch.Tensor]): lora'b weight
+        output_tensor (torch.Tensor): output tensor
+        token_lora_mapping (torch.Tensor): A tensor mapping each input token to the lora-id
+            related to that token. A value of -1 indicates that LoRA doesn't apply to that
+            token.
+        token_indices_sorted_by_lora_ids (torch.Tensor): Row/Token indices from the A matrix
+            grouped by LoRA IDs.
+        num_tokens_per_lora (torch.Tensor): num_tokens_per_lora[i] is the number of tokens
+            that are to be processed by LoRA ID lora_ids[i] 
+        lora_token_start_loc (torch.Tensor): A cumulative sum of num_tokens_per_lora.
+            lora_token_start_loc[0] is always 0 so that lora_token_start_loc[i],
+            along with num_tokens_per_lora[i] identifies the the region in
+            token_indices_sorted_by_lora_ids that LoRA lora_ids[i] should
+            process.
+        lora_ids (torch.Tensor): LoRA ids to process.
+        offset_start (int, optional): Offset start for output_tensor. 
+            Defaults to 0.
+        add_inputs (bool, optional): Whether to add the input tensor to the 
+            output tensor. Defaults to False.
+    """
+    assert inputs.dtype in [torch.float16, torch.bfloat16, torch.float32]
+    for weight in lora_b_weights:
+        assert weight.dtype in [torch.float16, torch.bfloat16]
+
+    assert inputs.size(0) == len(lora_b_weights)
+
+    assert output_tensor.is_contiguous()
+    (slice_start_tensor, lora_ptr_tensor, lora_strides_d0_tensor,
+     lora_strides_d1_tensor, lora_strides_d2_tensor, hidden_sizes_tensor,
+     same_stride, MAX_N) = _get_lora_b_ptr(lora_b_weights, offset_start,
+                                           inputs.device)
+
+    # TODO tuning this config
+    K = lora_b_weights[0].shape[-1]  # K= rank
+
+    M = inputs.size(1)
+    BLOCK_M = 64
+    BLOCK_N = 128
+    BLOCK_K = 16
+    EVEN_K = K % BLOCK_K == 0
+    ADD_INPUTS = add_inputs
+    MAX_LORAS = lora_ids.size(0)
+    CAST_TYPE = False
+
+    if inputs.dtype == torch.float32 and lora_b_weights[0].dtype in [
+            torch.float16,
+            torch.bfloat16,
+    ]:
+        CAST_TYPE = True
+
+
+    grid = (
+        MAX_LORAS * triton.cdiv(M, BLOCK_M) * triton.cdiv(MAX_N, BLOCK_N),
+        len(lora_b_weights),
+    )
+    _v1_expand_kernel[grid](
+        inputs,
+        lora_ptr_tensor,
+        output_tensor,
+        M,
+        MAX_N,
+        K,
+        # V1 new
+        token_indices_sorted_by_lora_ids,
+        num_tokens_per_lora,
+        lora_token_start_loc,
+        lora_ids,
+        # ---
+        slice_start_tensor,
+        inputs.stride(0),
+        inputs.stride(1),
+        inputs.stride(2),
+        lora_strides_d0_tensor,
+        lora_strides_d1_tensor,
+        lora_strides_d2_tensor,
+        output_tensor.stride(0),
+        output_tensor.stride(1),
+        hidden_sizes_tensor,
+        BLOCK_M,
+        BLOCK_N,
+        BLOCK_K,
+        EVEN_K,
+        ADD_INPUTS,
+        CAST_TYPE,
+        len(lora_b_weights),
+        same_stride,
+    )
+    return
+
+
+def _v1_expand_fake(
+    inputs: torch.Tensor,
+    lora_b_weights: List[torch.Tensor],
+    output_tensor: torch.Tensor,
+    token_lora_mapping: torch.Tensor,
+    token_indices_sorted_by_lora_ids: torch.Tensor,  # inputs.size(0)
+    num_tokens_per_lora: torch.Tensor,  # max-loras + 1
+    lora_token_start_loc: torch.Tensor,  # max-loras + 2
+    lora_ids: torch.Tensor,  # max-loras + 1
+    offset_start: int = 0,
+    add_inputs: bool = False,
+) -> None:
+    return
+
+
+try:
+    direct_register_custom_op(
+        op_name="v1_expand",
+        op_func=_v1_expand,
+        mutates_args=["output_tensor"],
+        fake_impl=_v1_expand_fake,
+    )
+    v1_expand = torch.ops.vllm.v1_expand
+
+except AttributeError:
+    v1_expand = _v1_expand
---
+++ vllm/lora/ops/triton_ops/v1_shrink.py
@@ -0,0 +1,248 @@
+"""
+Based on:
+Chen, L., Ye, Z., Wu, Y., Zhuo, D., Ceze, L., & Krishnamurthy, A. (2023). 
+Punica: Multi-Tenant LoRA Serving. 
+https://arxiv.org/abs/2310.18547
+"""
+
+from typing import List
+
+import torch
+import triton
+import triton.language as tl
+
+from vllm.utils import direct_register_custom_op
+
+from .utils import _get_lora_a_ptr
+from .kernel_utils import do_shrink_kernel
+
+
+
+@triton.jit
+def _v1_shrink_kernel(
+        input_ptr,
+        lora_ptr,  #1-3
+        out_ptr,
+        M,
+        N,
+        K,
+        token_indices_sorted_by_lora_ids,
+        num_tokens_per_lora,
+        lora_token_start_loc,
+        lora_ids,
+        scaling,
+        input_d0_stride,
+        input_d1_stride,  # 1
+        lora_d0_stride,
+        lora_d1_stride,
+        lora_d2_stride,  # 1
+        output_d0_stride,
+        output_d1_stride,
+        output_d2_stride,  # 1 
+        BLOCK_M: tl.constexpr,
+        BLOCK_N: tl.constexpr,
+        BLOCK_K: tl.constexpr,
+        EVEN_K: tl.constexpr,
+        SPLIT_K: tl.constexpr,
+        SLICE_NUM: tl.constexpr):
+    """
+    The sgmv's shrink triton kernel is based on GroupGEMM+SPLIT-K.
+    The GEMM of Multi-LoRA can be considered as GroupGEMM. Additionally,
+    introducing SPLIT-K can improve performance
+    """
+
+
+    pid_lmn = tl.program_id(axis=0)
+    pid_sk_slice = tl.program_id(axis=1)
+
+    cta_n_num = tl.cdiv(N, BLOCK_N)
+    cta_m_num = tl.cdiv(M, BLOCK_M)
+    lora_idx = pid_lmn // (cta_m_num * cta_n_num)
+    pid_n = (pid_lmn // cta_m_num) % cta_n_num
+    pid_m = pid_lmn % cta_m_num
+
+    lora_id = tl.load(lora_ids + lora_idx)
+    if lora_id == -1:
+        # early exit for the no-lora case.
+        return
+
+    if SLICE_NUM == 1:
+        slice_id: tl.constexpr = 0
+        pid_sk = pid_sk_slice 
+    else:
+        slice_id = pid_sk_slice // SPLIT_K
+        pid_sk = pid_sk_slice % SPLIT_K
+
+
+    # lora m indices offsets
+    lora_m_indices_start = tl.load(lora_token_start_loc + lora_idx)
+    lora_m_size = tl.load(num_tokens_per_lora + lora_idx)
+
+    cta_m_offset = pid_m * BLOCK_M
+    if cta_m_offset >= lora_m_size:
+        # early exit CTA
+        return
+
+    CTA_M_LEN = min(BLOCK_M, lora_m_size - cta_m_offset)
+    offset_m = tl.arange(0, BLOCK_M) % CTA_M_LEN 
+
+    cta_lora_seq_indices = (token_indices_sorted_by_lora_ids +
+                            lora_m_indices_start + cta_m_offset)
+    ram = tl.load(cta_lora_seq_indices + offset_m)
+
+    do_shrink_kernel(pid_m,
+                     pid_n,
+                     pid_sk,
+                     slice_id,
+                     lora_id,
+                     input_ptr,
+                     lora_ptr,
+                     out_ptr,
+                     N,
+                     K,
+                     CTA_M_LEN,
+                     ram,
+                     # input strides
+                     input_d0_stride,
+                     input_d1_stride,
+                     # lora strides
+                     lora_d0_stride,
+                     lora_d1_stride,
+                     lora_d2_stride,
+                     # output strides
+                     output_d0_stride,
+                     output_d1_stride,
+                     output_d2_stride,
+                     scaling,
+                     BLOCK_M,
+                     BLOCK_N,
+                     BLOCK_K,
+                     EVEN_K,
+                     SPLIT_K,
+                     SLICE_NUM)
+
+
+@torch.inference_mode()
+def _v1_shrink(
+    inputs: torch.Tensor,
+    lora_a_weights: List[torch.Tensor],
+    output_tensor: torch.Tensor,
+    token_lora_mapping: torch.Tensor,
+    token_indices_sorted_by_lora_ids: torch.Tensor,  # inputs.size(0)
+    num_tokens_per_lora: torch.Tensor,  # max-loras
+    lora_token_start_loc: torch.Tensor,  # max-loras
+    lora_ids: torch.Tensor,  # max-loras
+    scaling: float,
+) -> None:
+    """
+    Args:
+        inputs (torch.Tensor): input tensor
+        lora_a_weights (List[torch.Tensor]): lora'a weight
+        output_tensor (torch.Tensor): output tensor
+        b_seq_start_loc (torch.Tensor): (batch_size,). The cumulative
+            sequence lengths of the sequences in the batch, used to index
+            into sequence. E.g., if the sequence length is [4, 6], it is
+            [0, 4].
+        seq_len_tensor (torch.Tensor): (batch_size,). Record the sequence
+            length of the sequences in the batch.
+        lora_indices_tensor (torch.Tensor): (batch_size,). The LoRA index
+            corresponding to each batch. An index of -1 means no lora should be
+            applied.
+        batches (int): batch size
+        max_seq_length (int): The max sequence lengths of the sequences in the 
+            batch.
+        token_nums (int): The token numbers in the batch. Used to verify if the 
+            token numbers in the inputs matches the one in the metadata.
+        scaling (float): Scaling factor.
+    """
+    assert inputs.dtype == lora_a_weights[0].dtype
+    assert inputs.dtype in [torch.float16, torch.bfloat16]
+    for weight in lora_a_weights:
+        assert weight.dtype in [torch.float16, torch.bfloat16]
+
+    assert inputs.size(1) == lora_a_weights[0].size(-1)
+    assert inputs.is_contiguous()
+    assert output_tensor.is_contiguous()
+    (lora_ptr_tensor, lora_strides_d0, lora_strides_d1,
+     lora_strides_d2) = _get_lora_a_ptr(lora_a_weights, inputs.device)
+    # TODO tuning this config
+    N, K = lora_a_weights[0].shape[-2:]  # K=hidden_size,N=rank
+    M = inputs.size(0)
+    BLOCK_M = 32
+    BLOCK_N = 16
+
+    #BLOCK_K = 32
+    #SPLIT_K = 8
+
+    if M < 128:
+        BLOCK_K = 256
+        SPLIT_K = 64
+    else:
+        BLOCK_K = 32
+        SPLIT_K = 8
+
+
+    EVEN_K = K % (BLOCK_K * SPLIT_K) == 0
+    MAX_LORAS = lora_ids.size(0)
+    grid = (
+        MAX_LORAS * triton.cdiv(M, BLOCK_M) * triton.cdiv(N, BLOCK_N),
+        SPLIT_K * len(lora_a_weights),
+    )
+
+    _v1_shrink_kernel[grid](
+        inputs,
+        lora_ptr_tensor,
+        output_tensor,
+        M,
+        N,
+        K,
+        # New additions 
+        token_indices_sorted_by_lora_ids,
+        num_tokens_per_lora,
+        lora_token_start_loc,
+        lora_ids,
+        # ----
+        scaling,
+        inputs.stride(0),
+        inputs.stride(1),
+        lora_strides_d0,
+        lora_strides_d1,
+        lora_strides_d2,
+        output_tensor.stride(0),
+        output_tensor.stride(1),
+        output_tensor.stride(2),
+        BLOCK_M,
+        BLOCK_N,
+        BLOCK_K,
+        EVEN_K,
+        SPLIT_K,
+        len(lora_a_weights),
+    )
+    return
+
+
+def _v1_shrink_fake(
+    inputs: torch.Tensor,
+    lora_a_weights: List[torch.Tensor],
+    output_tensor: torch.Tensor,
+    token_lora_mapping: torch.Tensor,
+    token_indices_sorted_by_lora_ids: torch.Tensor,  # inputs.size(0)
+    num_tokens_per_lora: torch.Tensor,  # max-loras
+    lora_token_start_loc: torch.Tensor,  # max-loras
+    lora_ids: torch.Tensor,  # max-loras
+    scaling: float,
+) -> None:
+    return
+
+
+try:
+    direct_register_custom_op(
+        op_name="v1_shrink",
+        op_func=_v1_shrink,
+        mutates_args=["output_tensor"],
+        fake_impl=_v1_shrink_fake,
+    )
+    v1_shrink = torch.ops.vllm.v1_shrink
+
+except AttributeError:
+    v1_shrink = _v1_shrink
