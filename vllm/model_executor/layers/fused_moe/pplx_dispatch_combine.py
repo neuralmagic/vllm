a6be78d8d870f12ada7763b8ba2dd51566d81545
Bill Nell <bnell@redhat.com>
2025-04-10 14:47:37 +0000

    hook up some wires
    Signed-off-by: Bill Nell <bnell@redhat.com>

--- vllm/model_executor/layers/fused_moe/pplx_dispatch_combine.py
+++ vllm/model_executor/layers/fused_moe/pplx_dispatch_combine.py
@@ -9,7 +9,10 @@ from vllm.forward_context import get_forward_context
 from vllm.model_executor.layers.fused_moe.utils import _fp8_quantize
 from vllm.model_executor.layers.fused_moe.moe_align_block_size import (
     moe_align_block_size)
+from vllm.logger import init_logger
 
+
+logger = init_logger(__name__)
 
 # Note use: layer.get_all_to_all() to get an AllToAll instance
 # The max_num_tokens, world_size and dp_size must be the same
@@ -22,13 +25,16 @@ class PplxDispatchCombine(mk.FusedMoEQuantizeDispatchCombine):
                  max_num_tokens: int,
                  world_size: int,
                  dp_size: int,
+                 rank: int,
                  quant_dtype: Optional[torch.dtype] = None,
                  block_shape: Optional[List[int]] = None):
         super().__init__()
         self.a2a = a2a
         self.block_shape = block_shape
-        self.max_num_tokens = max_num_tokens
-        self.dp_num_tokens = max_num_tokens * (world_size // dp_size)
+        self.max_num_tokens = max_num_tokens                             # not right?
+        self.dp_num_tokens = max_num_tokens #* (world_size // dp_size)   # this is not right
+        self.world_size = world_size  # debug
+        self.rank = rank              # debug
         self.quant_dtype = quant_dtype
         print(f"max_num_tokens = {max_num_tokens}")
         print(f"dp_num_tokens = {self.dp_num_tokens}")
@@ -97,16 +103,6 @@ class PplxDispatchCombine(mk.FusedMoEQuantizeDispatchCombine):
         bound_m = None #get_forward_context().dp_metadata.dp_rank_num_tokens
 
         # TODO: optimize this?
-        #indices = rank_topk_ids.to(dtype=torch.uint32)
-
-        sorted_token_ids, indices, num_tokens_post_padded = (
-            moe_align_block_size(
-                rank_topk_ids,
-                1,
-                num_experts,
-                None
-            ))
-
         indices = rank_topk_ids.to(dtype=torch.uint32)
 
         # rank_topk_ids is (num_tokens, experts_per_token)
@@ -119,9 +115,20 @@ class PplxDispatchCombine(mk.FusedMoEQuantizeDispatchCombine):
         if expert_x_scale is not None:
             print(f"expert_x_scale = {expert_x_scale.shape}")
         print(f"bound_m = {bound_m}")
-        print(f"topk_ids = {rank_topk_ids.shape} {rank_topk_ids}")
+        #print(f"topk_ids = {rank_topk_ids.shape} {rank_topk_ids}")
         print(f"indices = {indices.shape} {torch.unique(indices.flatten())}")
 
+        #######
+        expert_token_from: list[list[tuple[int, int]]] = [
+            [] for _ in range(num_experts)
+        ]
+        i_rank = self.rank
+        for token_idx in range(a1q.shape[0]):
+            for expert_idx in indices[token_idx]:
+                expert_token_from[expert_idx].append((i_rank, token_idx))
+        #######
+
+        print("DISPATCH")
         self.a2a.dispatch(
             out_expert_num_tokens=expert_num_tokens,
             out_expert_x=expert_x,
@@ -141,6 +148,33 @@ class PplxDispatchCombine(mk.FusedMoEQuantizeDispatchCombine):
             if torch.isnan(expert_x[i]).sum() < expert_x[i].numel():
                 print(i)
         print("END_EXPERT_X")
+
+        num_local_experts = num_experts // self.world_size
+        for i_rank in range(self.world_size):
+            if i_rank != self.rank:
+                continue
+            for i_local_expert in range(num_local_experts):
+                expert_idx = i_rank * num_local_experts + i_local_expert
+                cnt_tokens = int(expert_num_tokens[i_local_expert].item())
+                logger.debug(
+                    "Expert #%d on Rank %d: %d tokens",
+                    expert_idx,
+                    self.rank,
+                    cnt_tokens,
+                )
+                #assert cnt_tokens == len(expert_token_from[expert_idx]), f"{cnt_tokens} != {len(expert_token_from[expert_idx])}"
+                #cnt_from_dp_rank = [0] * self.dp_num_tokens
+                for i_token in range(cnt_tokens):
+                    src_dp_rank, src_token_idx = expert_token_from[expert_idx][i_token]
+                    #cnt_from_dp_rank[src_dp_rank] += 1
+                    dst_x = expert_x[i_local_expert, i_token]
+                    logger.debug(
+                        "  x[%d] (from DP Rank %d Token %d): %s",
+                        i_token,
+                        src_dp_rank,
+                        src_token_idx,
+                        dst_x.cpu(),
+                    )
 
         #return expert_x, expert_x_scale
         return (expert_x.view(-1, expert_x.shape[-1]),
