f679c6164a1510151aec38483e3f879fa15bebf0
Dipika <dipikasikka1@gmail.com>
2025-05-14 20:36:08 -0400

    add script

--- run_fp4.py
+++ run_fp4.py
@@ -1,4 +1,6 @@
-# SPDX-License-Identifier: Apache-2.0
+
+import numpy
+import torch
 
 from vllm import LLM, SamplingParams
 
@@ -8,17 +10,14 @@ prompts = [
 ]
 
 # Create a sampling params object for greedy sampling
-sampling_params = SamplingParams(temperature=0.90,
-                                 max_tokens=40,
-                                 min_tokens=10)
-#llm = LLM('/home/dsikka/llm-compressor/examples/quantization_w4a16_fp4/TinyLlama-1.1B-Chat-v1.0-FP4', enforce_eager=True)
-llm = LLM(
-    "/home/dsikka/llm-compressor/examples/quantization_w4a16_fp4/Llama-3.1-8B-Instruct-FP4",
-    enforce_eager=True)
-
-#llm = LLM("nvidia/Llama-3.3-70B-Instruct-FP4", quantization='nvfp4', max_model_len=2048, enforce_eager=True)
-#llm = LLM("nm-testing/llama2.c-stories110M-FP4", enforce_eager=True)
+sampling_params = SamplingParams(temperature=0.80, top_p=0.95, max_tokens=40, min_tokens=10)
+#llm  = LLM('nm-testing/Llama-3.1-8B-Instruct-FP4-Weight')
+#llm = LLM("/home/dsikka/llm-compressor/examples/quantization_w4a16_fp4/TinyLlama-1.1B-Chat-v1.0-FP4")
+#llm = LLM("/home/dsikka/llm-compressor/examples/quantization_w4a16_fp4/Llama-3.1-8B-Instruct-NVFP4A16")
+#llm = LLM("/home/dsikka/llm-compressor/examples/quantization_w4a16_fp4/Llama-3.1-8B-Instruct-NVFP4A16-MSE")
+#llm = LLM("nm-testing/Llama-3.3-70B-Instruct-NVFP4A16", max_model_len=4096)
 # Print the outputs.
+llm = LLM("nvidia/Llama-3.3-70B-Instruct-FP4", max_model_len=4096, quantization="nvfp4", enforce_eager=True)
 output = llm.generate(prompts, sampling_params)
 for o in output:
     print(o.outputs[0].text)
