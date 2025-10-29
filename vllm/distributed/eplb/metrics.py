eadd1e8583a0049c74e4bb2eae5509ea1d2d0b92
Sage Moore <sage@neuralmagic.com>
2025-10-29 18:11:45 +0000

    init
    Signed-off-by: Sage Moore <sage@neuralmagic.com>

---
+++ vllm/distributed/eplb/metrics.py
@@ -0,0 +1,53 @@
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: Copyright contributors to the vLLM project
+
+from dataclasses import dataclass
+
+import prometheus_client
+
+from vllm.config import EPLBConfig
+from vllm.logger import init_logger
+
+logger = init_logger(__name__)
+
+
+# TODO: Move this to a common location with spec decode metrics
+def make_per_engine(
+    counter: prometheus_client.Counter, per_engine_labelvalues: dict[int, list[str]]
+):
+    """Create a counter for each label value."""
+    return {
+        idx: counter.labels(*labelvalues)
+        for idx, labelvalues in per_engine_labelvalues.items()
+    }
+
+
+@dataclass
+class EPLBStats:
+    counter_a: int = 0
+
+
+class EPLBProm:
+    _counter_cls = prometheus_client.Counter
+
+    def __init__(
+        self,
+        eplb_config: EPLBConfig | None,
+        labelnames: list[str],
+        per_engine_labelvalues: dict[int, list[str]],
+    ):
+        self.eplb_enabled = eplb_config is not None
+        if not self.eplb_enabled:
+            return
+
+        counter_a = self._counter_cls(
+            name="vllm:eplb_counter_a",
+            documentation="The first EPLB counter",
+            labelnames=labelnames,
+        )
+        self.counter_a = make_per_engine(counter_a, per_engine_labelvalues)
+
+    def observe(self, eplb_stats: EPLBStats, engine_idx: int = 0):
+        if not self.eplb_enabled:
+            return
+        self.counter_a[engine_idx].inc(eplb_stats.counter_a)
