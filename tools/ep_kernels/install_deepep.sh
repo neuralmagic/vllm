9968c462ad6cd37bd7fbececc8ab9cae5c93cc04
Elvir Crncevic <elvircrn@gmail.com>
2025-11-27 20:37:53 +0100

    deepep compile

---
+++ tools/ep_kernels/install_deepep.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -ex
+
+# prepare workspace directory
+WORKSPACE=$1
+if [ -z "$WORKSPACE" ]; then
+    export WORKSPACE=$(pwd)/ep_kernels_workspace
+fi
+
+# build and install deepep, require pytorch installed
+pushd $WORKSPACE
+cd DeepEP
+git pull origin main
+export NVSHMEM_DIR=$WORKSPACE/nvshmem_install
+$PIP_CMD install --no-build-isolation -vvv -e .
+popd
