553dce24670f21e01eb3a438de15370d72226abc
Elvir Crncevic <elvircrn@gmail.com>
2025-12-04 11:27:44 +0100

    log failed bench serve requests

--- vllm/benchmarks/serve.py
+++ vllm/benchmarks/serve.py
@@ -382,6 +382,10 @@ def calculate_metrics(
     # Find the time range across all successful requests
     successful_outputs = [output for output in outputs if output.success]
     failed_outputs = [output for output in outputs if not output.success]
+
+    if len(failed_outputs) > 0:
+        print(f'===== failed outputs[0] = {failed_outputs[0].error} =====')
+
     if successful_outputs:
         min_start_time = min(output.start_time for output in successful_outputs)
         max_end_time = max(
