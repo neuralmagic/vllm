405fea7eadc109c8996817e91c3882c91ab47ce6
Bill Nell <bnell@redhat.com>
2026-01-14 18:27:54 -0500

    make gate into a property
    Signed-off-by: Bill Nell <bnell@redhat.com>

--- vllm/model_executor/layers/fused_moe/layer.py
+++ vllm/model_executor/layers/fused_moe/layer.py
@@ -337,8 +337,11 @@ class FusedMoE(CustomOp):
         n_shared_experts: int | None = None,
         routing_method_type: RoutingMethodType | None = None,
         router_logits_dtype: torch.dtype | None = None,
+        gate: torch.nn.Module | None = None,
     ):
         super().__init__()
+
+        self._gate = gate
 
         if params_dtype is None:
             params_dtype = torch.get_default_dtype()
@@ -627,7 +630,7 @@ class FusedMoE(CustomOp):
             layer=self,
             moe_config=self.moe_config,
             router=router,
-            # gate=self.gate,
+            gate=self.gate,
             # shared_experts=self.shared_experts,
             quant_method=quant_method,
             reduce_results=self.reduce_results,
@@ -672,7 +675,7 @@ class FusedMoE(CustomOp):
 
     @property
     def gate(self) -> torch.nn.Module | None:
-        return None
+        return self._gate
 
     @property
     def tp_size(self):
@@ -1493,15 +1496,13 @@ class FusedMoE(CustomOp):
 # TODO
 # make gate, shared_experts into properties instead of methods
 #
-
-
 class DefaultMoERunner(MoERunner):
     def __init__(
         self,
         layer: FusedMoE,  # TODO: turn into generic module/parameter blob
         moe_config: FusedMoEConfig,
         router: FusedMoERouter,
-        # gate: torch.nn.Module | None,
+        gate: torch.nn.Module | None,
         # shared_experts: torch.nn.Module | None,
         quant_method: FusedMoEMethodBase,
         reduce_results: bool,
@@ -1509,10 +1510,10 @@ class DefaultMoERunner(MoERunner):
         capture: Callable[[torch.Tensor], None] | None = None,
     ):
         super().__init__()
-        self.layer = layer
+        self.layer = layer  # TODO: get rid of this
         self.moe_config = moe_config
         self.router = router
-        # self.gate = gate
+        self.gate = gate
         # self.shared_experts = shared_experts
         self.quant_method = quant_method
         self.reduce_results = reduce_results
@@ -1598,11 +1599,6 @@ class DefaultMoERunner(MoERunner):
     @property
     def shared_experts(self) -> torch.nn.Module | None:
         return self.layer.shared_experts
-
-    # TBD
-    @property
-    def gate(self) -> torch.nn.Module | None:
-        return self.layer.gate
 
     def ensure_moe_quant_config_init(self):
         if self.quant_method.moe_quant_config is None:
