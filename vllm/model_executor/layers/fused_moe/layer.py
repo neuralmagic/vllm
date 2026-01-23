c80b23a5368206cec5e825c2534c116a42f29000
Bill Nell <bnell@redhat.com>
2026-01-23 11:02:17 -0500

    fixes
    Signed-off-by: Bill Nell <bnell@redhat.com>

--- vllm/model_executor/layers/fused_moe/layer.py
+++ vllm/model_executor/layers/fused_moe/layer.py
@@ -551,7 +551,7 @@ class FusedMoE(CustomOp):
             device=vllm_config.device_config.device,
             routing_method=self.routing_method_type,
         )
-        if self.use_mori_kernels:
+        if self.moe_config.use_mori_kernels:
             assert self.rocm_aiter_fmoe_enabled, (
                 "Mori needs to be used with aiter fused_moe for now."
             )
@@ -1361,7 +1361,7 @@ class FusedMoE(CustomOp):
         assert all(
             weight.is_contiguous()
             for name, weight in weights
-            if not (name.startswith("_shared_experts.") or name.startswith("_gate"))
+            if not (name.startswith("_shared_experts.") or name.startswith("_gate."))
         )
 
         # Filter out the non-expert weights.
